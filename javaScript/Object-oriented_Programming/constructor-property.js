8619a54aec34d1804e9f59da3a32023826750732
elyse502 <elyseniyibizi502@gmail.com>
2024-04-30 11:40:30 +0300

    add Constructor descrpiption

--- javaScript/Object-oriented_Programming/constructor-property.js
+++ javaScript/Object-oriented_Programming/constructor-property.js
@@ -22,3 +22,26 @@
 new String(); // '', "", ``
 new Boolean(); // true, false
 new Number(); // 1, 2, 3, ...
+
+
+// In JavaScript Functions are objects
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    }
+}
+
+const Circle1 = new Function('radius', `
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    }
+`); // new Function() is not recommended
+
+const circle = new Circle1(1); // new Function() is not recommended
+
+Circle.call({}, 1); // {} is the value of this
+Circle.apply({}, [1]); // {} is the value of this
+
+const another = new Circle(1);
