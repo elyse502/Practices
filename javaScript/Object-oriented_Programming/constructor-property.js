89edd3f9d935c127e1e34e86ba7d588c45702286
elyse502 <elyseniyibizi502@gmail.com>
2024-04-30 11:06:39 +0300

    add Constructor descrpiption

--- javaScript/Object-oriented_Programming/constructor-property.js
+++ javaScript/Object-oriented_Programming/constructor-property.js
@@ -1,25 +1,24 @@
 /**
  * Every object in JavaScript has a constructor property that references the function that was used to create that object.
- * 
+ * - The constructor property is a reference to the function that was used to create an object
+ * - The constructor property is available on all objects
+ * - The constructor property is available on all objects created with a constructor function
+ * - The constructor property is available on all objects created with a factory function
+ * - The constructor property is available on all built-in objects
+ * - The constructor property is available on all primitive values
+ * - The constructor property is available on all functions
+ * - The constructor property is available on all arrays
+ * - The constructor property is available on all regular expressions
+ * - The constructor property is available on all dates
+ * - The constructor property is available on all numbers
+ * - The constructor property is available on all strings
+ * - The constructor property is available on all booleans
+ * - The constructor property is available on all symbols
+ * - The constructor property is available on all maps
+ * - The constructor property is available on all sets
  */
 
 
-// Factory Function
-function createCircle(radius) {
-    return {
-        radius,
-        draw: function() {
-            console.log('draw');
-        }
-    };
-}
-const circle = createCircle(1);
-
-// Constructor Function
-function Circle(radius) {
-    this.radius = radius;
-    this.draw = function() {
-        console.log('draw');
-    }
-}
-const another = new Circle(1);
+new String(); // '', "", ``
+new Boolean(); // true, false
+new Number(); // 1, 2, 3, ...
