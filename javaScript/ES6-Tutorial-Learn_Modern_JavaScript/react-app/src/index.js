0dafd72556360d383f5bd1b3c5b6d0ff36bf7db1
elyse502 <elyseniyibizi502@gmail.com>
2024-05-02 12:44:19 +0300

    Add all learnt concept today

--- javaScript/ES6-Tutorial-Learn_Modern_JavaScript/react-app/src/index.js
+++ javaScript/ES6-Tutorial-Learn_Modern_JavaScript/react-app/src/index.js
@@ -1,3 +1,7 @@
+// var -> function-scoped
+// let -> block-scoped
+// const -> block-scoped
+
 
 function sayHello() {
   for (let i = 0; i < 5; i++) {
@@ -6,3 +10,27 @@ function sayHello() {
 }
 
 sayHello();
+
+
+// The difference between let and const is that const is constant and cannot be changed
+const x = 1;
+// x = 2; // This will throw an error
+console.log(x);
+
+
+// Objects are reference types
+const person = {
+  name: 'Elysee',
+  walk() {},
+  talk() {}
+};
+
+person.talk();
+
+person['name'] = 'John'; // This is how you can change the value of a property in an object
+person.name = ''; // This is another way to change the value of a property in an object
+
+// This is how you can access a property in an object using a variable
+const targetMember = 'name';
+person[targetMember.value] = 'Eric';
+console.log(person.name); // Eric
