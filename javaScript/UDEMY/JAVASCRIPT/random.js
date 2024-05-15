d378ee2bb94c0902193c66c9e1979a1e2e7d0bb7
elyse502 <elyseniyibizi502@gmail.com>
2024-05-15 16:11:16 +0200

    Initial Commit

---
+++ javaScript/UDEMY/JAVASCRIPT/random.js
@@ -0,0 +1,11 @@
+const randomValue = Math.random();
+console.log("random value : ", randomValue);
+
+
+const randomInteger = Math.floor(Math.random() * 10) + 1;
+console.log("random integer (1-10): ", randomInteger);
+
+
+const diceRoll = Math.floor(Math.random() * 6) + 1;
+
+console.log("dice roll (1-6): ", diceRoll);
