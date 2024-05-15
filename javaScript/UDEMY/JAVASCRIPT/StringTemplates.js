07c9da16fe1936795fee69010011107812dd340c
elyse502 <elyseniyibizi502@gmail.com>
2024-05-15 15:54:10 +0200

    Initial Commit

---
+++ javaScript/UDEMY/JAVASCRIPT/StringTemplates.js
@@ -0,0 +1,14 @@
+/*let name = "javascript";
+let greeting = 'hello, ${name}';*/
+
+/*let a = 5;
+let b = 10;
+let result = 'the sum of ${a} and ${b} is ${a + b}';*/
+
+let multiline = `
+this is a multiline
+it allows for easy formatted
+no need for char
+`;
+
+console.log(multiline);
