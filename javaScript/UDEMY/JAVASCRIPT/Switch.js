ed8929b9d3d6a3ece24796fd580edd72ea439e9e
elyse502 <elyseniyibizi502@gmail.com>
2024-05-15 16:54:11 +0200

    Initial Commit

---
+++ javaScript/UDEMY/JAVASCRIPT/Switch.js
@@ -0,0 +1,58 @@
+/*const dayOfWeek = "tuesday";
+
+switch (dayOfWeek) {
+    case "monday":
+        console.log("I hate mondays");
+        break;
+    case "tuesday":
+        console.log("Taco Tuesday");
+        break;
+    case "wednesday":
+        console.log("Hump day");
+        break;
+    case "thursday":
+        console.log("Thirsty Thursday");
+        break;
+    case "friday":
+        console.log("TGIF");
+        break;
+    default:
+        console.log("It's a weekend");
+        break;
+}*/
+
+
+/*const dayOfWeek = "wednesday";
+
+switch (dayOfWeek) {
+    case "monday":
+        case "tuesday":
+            case "wednesday":
+                case "thursday":
+                    console.log("it's a weekday");
+                    break;
+                case "friday":
+                    console.log("weekend is almost here");
+                    break;
+                default:
+                    console.log("It's a weekend");
+                    break;
+}*/
+
+
+const value = "Elysee";
+
+switch (typeof value) {
+    case "number":
+        console.log("It's a number");
+        break;
+    case "string":
+        console.log("It's a string");
+        break;
+    case "boolean":
+        console.log("It's a boolean");
+        break;
+    default:
+        console.log("It's something else");
+        break;
+}
