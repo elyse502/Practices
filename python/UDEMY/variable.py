79d8acd87918b5fe5fb76ca32e89ac39adc93796
elyse502 <elyseniyibizi502@gmail.com>
2024-05-16 19:49:35 +0200

    Add all commpleted Lessons during the Lecture

---
+++ python/UDEMY/variable.py
@@ -0,0 +1,20 @@
+# Declaring and assigning value to constants
+PI = 3.14
+GRAVITY = 9.8
+
+# Declaring and assigning value to a variable
+a = "Apple"
+print(a)
+
+# Changing value of a variable
+a = "Aeroplane"
+print(a)
+
+a = 100
+print(a)
+
+# Assigning Multiple values to a variable
+b, c, d = 1, 2.5, "Hello"
+print(b, c, d)
+
+# Assigning same value to multiple variables
