64a9dc8514b7181bb8698ebc40cf2d67c6d25f82
elyse502 <elyseniyibizi502@gmail.com>
2024-05-17 09:56:05 +0200

    Add all completed Lessons during the Lecture

---
+++ python/UDEMY/module.py
@@ -0,0 +1,15 @@
+# Importing module as well as renaming it
+import math as m
+
+print ("The value of pi is", m.pi)
+
+
+# Usage of from
+from math import pi
+print("The value of pi is", pi)
+
+
+# import all names from the standard module math
+from math import *
+print("The value of e is", e)
+print("The value of pi is", pi)
