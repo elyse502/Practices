92497ea76e3e9b35a31445eafa72e4bc8f90ff07
elyse502 <elyseniyibizi502@gmail.com>
2024-05-21 08:54:12 +0200

    Initial Commit

--- python/UDEMY/oop.py
+++ python/UDEMY/oop.py
@@ -61,3 +61,29 @@ pg1 = myPenguin()
 pg1.whoisThis()
 pg1.canSwim()
 pg1.canRun()
+
+
+# Another part
+# DAta Encapsulation
+class personalComputer:
+
+    def __init__(self):
+        self.maxComputerPrice = 20000
+
+    def mySell(self):
+        print("Selling Price: {}".format(self.maxComputerPrice))
+
+    def setMaxComputerPrice(self, price):
+        self.maxComputerPrice = price
+
+
+pc = personalComputer()
+pc.mySell()
+
+# change the price
+pc.maxComputerPrice = 30000
+pc.mySell()
+
+# using setter function
+pc.setMaxComputerPrice(40000)
+pc.mySell()
