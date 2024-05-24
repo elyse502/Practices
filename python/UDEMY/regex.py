959856d6725b9c4e454bf32ed4e61adafdabe823
elyse502 <elyseniyibizi502@gmail.com>
2024-05-24 20:29:50 +0200

    Add all completed Lessons during the Lecture

--- python/UDEMY/regex.py
+++ python/UDEMY/regex.py
@@ -222,3 +222,8 @@ print()
 # 3. 2 to 20 lowercase and uppercase letters, numbers, plus .-
 # 4. A period
 # 5. 2 to 3 lowercase and uppercase letters
+
+emailList = "db@aol.com m@.com @apple.com db@.com"
+
+print("Email Matches :", len(re.findall("[\w._%+-]{1,20}@[\w.-]{2,20}.[A-Za-z]{2,3}", emailList)))
+print("Email Matches :", re.findall("[\w._%+-]{1,20}@[\w.-]{2,20}.[A-Za-z]{2,3}", emailList))
