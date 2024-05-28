09067770ba8a51fc8ed71a9e3c793e19673aaa1a
elyse502 <elyseniyibizi502@gmail.com>
2024-05-28 19:28:38 +0200

    Initial Commit

---
+++ python/UDEMY/flask/cookie.py
@@ -0,0 +1,27 @@
+#!/home/elysee_niyibizi/Practices/python/UDEMY/flask/flaskenv/bin/python3
+# Tutorial 55-Flask Framework Cookies
+
+from flask import Flask, render_template, request, make_response
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('setcookie.html')
+
+@app.route('/setcookie', methods = ['POST', 'GET'])
+def setcookie():
+    if request.method == 'POST':
+        user = request.form['nm']
+        resp = make_response(render_template('readcookie.html'))
+        resp.set_cookie('userID', user)
+        
+        return resp
+    
+@app.route('/getcookie')
+def getcookie():
+    name = request.cookies.get('userID')
+    return '<h1>welcome '+name+'</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
