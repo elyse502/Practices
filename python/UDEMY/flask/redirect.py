505f8a0fc38a9d348b44730bdc83516286de81fb
elyse502 <elyseniyibizi502@gmail.com>
2024-05-29 19:48:08 +0200

    Initial Commit

--- python/UDEMY/flask/redirect.py
+++ python/UDEMY/flask/redirect.py
@@ -8,3 +8,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('login.html')
+
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST' and request.form['username'] == 'admin' :
+        return redirect(url_for('success'))
+    return redirect(url_for('index'))
+
+@app.route('/success')
+def success():
+    return 'logged in successfully'
+
+if __name__ == '__main__':
+    app.run(debug = True)
