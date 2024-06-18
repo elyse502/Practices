cf95e40e86477b282505f8eb38f1cb802ccfe0bf
elyse502 <elyseniyibizi502@gmail.com>
2024-06-18 21:15:19 +0300

    Add all about React Components And Templates

---
+++ nodeJs/react/my_webapp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div calssName="content"></div>
+      <h1>App Component</h1>
+    </div>
+  );
+}
+
+export default App;
