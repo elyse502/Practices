d55252de63c291d99c6f602078477727a6af55c9
elyse502 <elyseniyibizi502@gmail.com>
2024-06-24 10:26:08 +0300

    Store counting in a Local Storage

--- nodeJs/react/web-app/src/App.jsx
+++ nodeJs/react/web-app/src/App.jsx
@@ -1,16 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const storageKeyName = "myotherkey";
+  
+  const retrieveCountValue = () => Number(localStorage.getItem(storageKeyName) || 0);
+
+  const [count, setCount] = useState(retrieveCountValue());
+  
+  const addNumber = (count) => setCount(Number(count) + 1);
+  
+
+    useEffect(() => {
+        localStorage.setItem(storageKeyName, String(count));
+    }, [count]);
 
   return (
     <>
       <div className='App'>
         Hello World!
-        <button onClick={() => setCount((count) => count + 1)}>Count is {count}</button>
+        <button onClick={() => addNumber(count)}>Count is {count}</button>
       </div>
     </>
   )
