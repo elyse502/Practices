72c6911b1a8ead3b5256ea5b9330dec5914bb9cb
elyse502 <elyseniyibizi502@gmail.com>
2024-06-28 14:16:31 +0300

    Initial Commit

--- python/UDEMY/flask/cookie.py
+++ python/UDEMY/flask/cookie.py
@@ -1,4 +1,4 @@
-#!/home/elysee_niyibizi/Practices/python/UDEMY/flask/flaskenv/bin/python3
+#!/usr/bin/env python3
 # Tutorial 55-Flask Framework Cookies
 
 from flask import Flask, render_template, request, make_response
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def index():
     return render_template('setcookie.html')
 
-@app.route('/setcookie', methods = ['POST', 'GET'])
+@app.route('/setcookie', methods=['POST', 'GET'])
 def setcookie():
     if request.method == 'POST':
         user = request.form['nm']
@@ -19,18 +19,9 @@ def setcookie():
     
 @app.route('/getcookie')
 def getcookie():
-    """
-    Get the value of the 'userID' cookie from the request and return a welcome message if the cookie exists,
-    otherwise return a message indicating that no cookie was found.
-
-    :return: A string containing a welcome message if the 'userID' cookie exists, otherwise a message indicating
-             that no cookie was found.
-    """
     name = request.cookies.get('userID')
-    if name:
-        return '<h1>welcome {}</h1>'.format(name)
-    else:
-        return '<h1>No cookie found</h1>'
+    return '<h1>Welcome, '+name+'!</h1>'
+
 
 if __name__ == '__main__':
     app.run(debug = True)
