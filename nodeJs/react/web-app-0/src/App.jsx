310c18abf06f1abfa5804737294aca30e9cb9785
elyse502 <elyseniyibizi502@gmail.com>
2024-07-03 13:56:55 +0200

    Add some cool styling with styled-components 🎉

--- nodeJs/react/web-app-0/src/App.jsx
+++ nodeJs/react/web-app-0/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import { MainApp } from './styled';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  
+  const storageKeyName = "count";
+  
+  const retrieveCountValue = () => Number(localStorage.getItem(storageKeyName) || 0);
+
+  const [count, setCount] = useState(retrieveCountValue());
+  const addNumber = (count) => setCount(Number(count) + 1);
+  
+    useEffect(() => {
+        localStorage.setItem(storageKeyName, String(count));
+    }, [count]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <MainApp>
+        Count Me
+
+        <button onClick={() => addNumber(count)}>
+          Count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </MainApp>
   )
 }
 
-export default App
+export default App;
