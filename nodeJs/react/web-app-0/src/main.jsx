1363902477acbca39d03927a4f7c5d1fe1646895
elyse502 <elyseniyibizi502@gmail.com>
2024-07-03 17:39:18 +0200

    Chakra UI changes; Better UX

--- nodeJs/react/web-app-0/src/main.jsx
+++ nodeJs/react/web-app-0/src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
 import App from './App.jsx'
-import './index.css'
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#f7fafc",
+      // ...
+      900: "#1a202c",
+    },
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
