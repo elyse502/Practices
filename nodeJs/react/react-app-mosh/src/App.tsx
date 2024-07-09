ad06dc48864b0ad53655af7c00f959dee02fa364
elyse502 <elyseniyibizi502@gmail.com>
2024-07-09 19:49:09 +0200

    Use Fragmens 🧷

--- nodeJs/react/react-app-mosh/src/App.tsx
+++ nodeJs/react/react-app-mosh/src/App.tsx
@@ -2,13 +2,9 @@ import ListGroup from "./components/ListGroup";
 
 function App() {
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <div>
+      <ListGroup />
+    </div>
   );
 }
 
