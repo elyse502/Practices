0f63620aa34cdbdf10f091877589a46d43f1f6d0
elyse502 <elyseniyibizi502@gmail.com>
2024-07-09 19:34:56 +0200

    Create a ListGroup Component

--- nodeJs/react/react-app-mosh/src/App.tsx
+++ nodeJs/react/react-app-mosh/src/App.tsx
@@ -1,7 +1,15 @@
-import Message from './Message';
+import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><Message/></div>
+  return (
+    <ul className="list-group">
+      <li className="list-group-item">An item</li>
+      <li className="list-group-item">A second item</li>
+      <li className="list-group-item">A third item</li>
+      <li className="list-group-item">A fourth item</li>
+      <li className="list-group-item">And a fifth one</li>
+    </ul>
+  );
 }
 
 export default App;
--- nodeJs/react/react-app-mosh/src/Message.tsx
+++ nodeJs/react/react-app-mosh/src/Message.tsx
@@ -2,7 +2,7 @@
 // PascalCasing
 function Message() {
     // JSX: JavaScript XML
-    const name = 'Elysée';
+    const name = '';
     if (name)
         return <h1>Hello {name}</h1>;
     return <h1>Hello World</h1>;
