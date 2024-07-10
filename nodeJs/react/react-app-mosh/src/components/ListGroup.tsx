3b0185ef44cfe51bcdc4e8e5e85c883253ba8810
elyse502 <elyseniyibizi502@gmail.com>
2024-07-10 15:16:12 +0200

    Use Proper Conditional Reandering ⚓

--- nodeJs/react/react-app-mosh/src/components/ListGroup.tsx
+++ nodeJs/react/react-app-mosh/src/components/ListGroup.tsx
@@ -1,9 +1,12 @@
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
 
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 ? <p>No items</p> : null}
+      {items.length === 0 && <p>No items</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
