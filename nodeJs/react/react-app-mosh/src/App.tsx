8b4732344807cd34725f33240aaca85915d930db
elyse502 <elyseniyibizi502@gmail.com>
2024-07-10 16:40:34 +0200

    Add all about Passing Data Via Props

--- nodeJs/react/react-app-mosh/src/App.tsx
+++ nodeJs/react/react-app-mosh/src/App.tsx
@@ -1,10 +1,11 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
   return (
     <div>
-      <ListGroup />
-      <ListGroup />
+      <ListGroup items={items} heading="Cities" />
     </div>
   );
 }
