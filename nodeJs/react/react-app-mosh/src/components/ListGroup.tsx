325dcd71507bb7204c815067b2997e05deb283dd
elyse502 <elyseniyibizi502@gmail.com>
2024-07-10 16:09:49 +0200

    Handle Events 🏎

--- nodeJs/react/react-app-mosh/src/components/ListGroup.tsx
+++ nodeJs/react/react-app-mosh/src/components/ListGroup.tsx
@@ -1,14 +1,20 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
+
+  // Event handler
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No items</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
