dbd7ebe85c0b3762777da595692428ffc8247d75
elyse502 <elyseniyibizi502@gmail.com>
2024-07-10 16:27:12 +0200

    Add all about Managing State 🎌

--- nodeJs/react/react-app-mosh/src/components/ListGroup.tsx
+++ nodeJs/react/react-app-mosh/src/components/ListGroup.tsx
@@ -1,10 +1,8 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  // Event handler
-  const handleClick = (event: MouseEvent) => console.log(event);
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
@@ -12,7 +10,17 @@ function ListGroup() {
       {items.length === 0 && <p>No items</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
