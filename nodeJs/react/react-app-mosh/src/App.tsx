ab9bb92bdaa2d415fb394be5ef7008519a51bbee
elyse502 <elyseniyibizi502@gmail.com>
2024-07-11 16:10:01 +0200

    Add Solution of an Exercise: Showing an Alert 🎯

--- nodeJs/react/react-app-mosh/src/App.tsx
+++ nodeJs/react/react-app-mosh/src/App.tsx
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alerVisible, setAlertVisibility] = useState(false);
+
   return (
     <div>
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
-      <Button color="secondary" onClick={() => console.log("Clicked")}>
+      {alerVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
+      )}
+      <Button color="secondary" onClick={() => setAlertVisibility(true)}>
         My Button
       </Button>
     </div>
