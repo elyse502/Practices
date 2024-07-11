963a3d9380a59f542b7a29cd4c7ac34602253f99
elyse502 <elyseniyibizi502@gmail.com>
2024-07-11 15:48:31 +0200

    Build a Button Component from Scratch 🗼

--- nodeJs/react/react-app-mosh/src/App.tsx
+++ nodeJs/react/react-app-mosh/src/App.tsx
@@ -1,9 +1,15 @@
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
   return (
     <div>
-      <Alert>Hello <span>World</span></Alert>
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
+      <Button color="secondary" onClick={() => console.log("Clicked")}>
+        My Button
+      </Button>
     </div>
   );
 }
