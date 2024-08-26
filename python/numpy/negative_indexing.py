6fd1270881ebc15820fff248358055dd29daec6f
elyse502 <elyseniyibizi502@gmail.com>
2024-08-26 09:50:00 +0300

    Add all about Using Negative Indexing to access elements in 1D Array

---
+++ python/numpy/negative_indexing.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+a = np.array([1,2,3,4,5])
+print(a[-1])  # a[4] = a[-1] = 5
+print(a[-2])  # a[3] = a[-2] = 4
+
+a = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(a[-1])
+print(a[-2])
