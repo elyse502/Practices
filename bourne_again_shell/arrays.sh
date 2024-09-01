1fb34afa5a99bfbc3593af371e58db579d170a72
elyse502 <elyseniyibizi502@gmail.com>
2024-09-01 22:04:45 +0300

    Add all about Working with Arrays

---
+++ bourne_again_shell/arrays.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Declaring an array
+fruits=("Apple" "Banana" "Orange" "Grapes")
+
+# Accessing array elements
+echo "First fruit: ${fruits[0]}"
+echo "Second fruit: ${fruits[1]}"
+
+# Length of the array
+echo "Number of fruits: ${#fruits[@]}"
+
