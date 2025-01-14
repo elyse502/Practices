5cf384ae955106871074d63dac140514964d8949
elyse502 <elyseniyibizi502@gmail.com>
2025-01-14 11:43:28 +0200

    Create To-Do list app

--- react/to-do-list/src/Components/Todo.jsx
+++ react/to-do-list/src/Components/Todo.jsx
@@ -1,11 +1,48 @@
+import { useState } from "react";
 import "./CSS/Todo.css";
+import { useRef } from "react";
+import { useEffect } from "react";
 
+let count = 0;
 const Todo = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef(null);
 
-export default Todo
+  const add = () => {
+    setTodos([
+      ...todos,
+      { no: count++, text: inputRef.current.value, display: "" },
+    ]);
+    inputRef.current.value = "";
+  };
+
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
+
+  return (
+    <div className="todo">
+      <div className="todo-header">To-Do List</div>
+      <div className="todo-add">
+        <input
+          ref={inputRef}
+          type="text"
+          placeholder="Add Your Task"
+          className="todo-input"
+        />
+        <div
+          onClick={() => {
+            add();
+          }}
+          className="todo-add-btn"
+        >
+          ADD
+        </div>
+      </div>
+
+      <div className="todo-list"></div>
+    </div>
+  );
+};
+
+export default Todo;
