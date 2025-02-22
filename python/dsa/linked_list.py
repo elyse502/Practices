3e7960cc3e7eb4d863ea8714668001405bf0e8ab
elyse502 <elyseniyibizi502@gmail.com>
2025-02-22 09:42:38 +0200

    Add how to delete a specific node from a singly linked list

--- python/dsa/linked_list.py
+++ python/dsa/linked_list.py
@@ -29,10 +29,24 @@ class LinkedList:
             current = current.next
         print("None")
 
+    def delete(self, data):
+        if not self.head:
+            return  # The list is empty
+        if self.head.data == data:
+            self.head = self.head.next  # Remove the head node
+            return
+        current = self.head
+        while current.next and current.next.data != data:
+            current = current.next
+        if current.next:
+            current.next = current.next.next  # Skip the node to delete
+
 
 # Example usage
 ll = LinkedList()
 ll.append(10)
 ll.append(20)
 ll.append(30)
+ll.display()
+ll.delete(20)
 ll.display()
