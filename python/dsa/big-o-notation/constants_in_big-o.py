2d386b060f96d426a3f28569eb10aeda50d72af8
elyse502 <elyseniyibizi502@gmail.com>
2025-03-24 14:06:21 +0200

    Add Constants in Big O

---
+++ python/dsa/big-o-notation/constants_in_big-o.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Time complexity of the 
+following code is O(2n) = O(n)
+"""
+def loop_twice(arr):
+    arr_copy_1 = list()
+    arr_copy_2 = list()
+
+    for i in arr:
+        print(i)
+        arr_copy_1.append(i)
+
+    for i in arr:
+        print(i)
+        arr_copy_2.append(i)
+
+input_arr = [1, 2, 3]
+loop_twice(input_arr)
