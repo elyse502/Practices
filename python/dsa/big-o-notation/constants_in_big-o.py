ed38afa7ecb28c3526bd8b49860accff9c8b2608
elyse502 <elyseniyibizi502@gmail.com>
2025-03-24 14:17:04 +0200

    New changes

--- python/dsa/big-o-notation/constants_in_big-o.py
+++ python/dsa/big-o-notation/constants_in_big-o.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """Time complexity of the 
 following code is O(2n) = O(n)
+        AND
+Space complexity of the 
+following code is O(2n) = O(n) too.
 """
 def loop_twice(arr):
     arr_copy_1 = list()
