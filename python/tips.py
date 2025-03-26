eced7bcd96483d3dab5675238da756f2e12258ef
elyse502 <elyseniyibizi502@gmail.com>
2025-03-26 11:32:33 +0200

    Add Dict technique

--- python/tips.py
+++ python/tips.py
@@ -9,3 +9,12 @@ print()
 m = 5
 for i in range(m):
 	print("* " * m if i in [0, m-1] else "* " + "  " * (m-2) + "*")
+
+"""Dictionary Technique"""
+def get_day(num):
+	days = {1: "Mon", 2: "Tue", 3: "Wedn"}
+	return days.get(num, "Invalid")
+
+print()
+print(get_day(1))
+print(get_day(4))
