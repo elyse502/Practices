2e34287bf04c77d764b02aba6ef22c373358e4fe
elyse502 <elyseniyibizi502@gmail.com>
2025-03-28 07:32:24 +0200

    Add Doubly Linked List Class Implementation

---
+++ python/dsa/doubly-linked-lists/doubly-linked_list_class_implementation.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
