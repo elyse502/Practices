dd0176016d93bbbd48540575c2114084b6b79a82
elyse502 <elyseniyibizi502@gmail.com>
2025-04-03 10:00:04 +0200

    Add Queue Implementation: Queue and Node Classes

---
+++ python/dsa/queues/queue_class-implementation.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Queue:
+    def __init__(self):
+        self.front = None
+        self.back = None
+        self.size = 0
+        
