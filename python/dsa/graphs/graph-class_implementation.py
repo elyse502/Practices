4e0d0608477aa535834c964d2f32296be343cea3
elyse502 <elyseniyibizi502@gmail.com>
2025-04-14 08:45:29 +0200

    Add Graph Implementation: Class & Constructor

---
+++ python/dsa/graphs/graph-class_implementation.py
@@ -0,0 +1,14 @@
+class Graph:
+    def __init__(self):
+        # Initialize an empty dictionary to store the adjacency list.
+        self.adjacency_list = {}
+
+    def add_node(self, node):
+        pass
+
+    def add_edge(self, node1, node2):
+        pass
+
+    def display(self):
+        pass
+    
