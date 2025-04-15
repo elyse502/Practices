cfa34261befcd1dfff2c262326bcfe1cb1a2435a
elyse502 <elyseniyibizi502@gmail.com>
2025-04-15 07:31:13 +0200

    Add Trie Implementation: Class

---
+++ python/dsa/tries/trie-class_implementation.py
@@ -0,0 +1,15 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        pass
+
+    def search(self, word):
+        pass
