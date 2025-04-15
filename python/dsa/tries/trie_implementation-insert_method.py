ff3937dfd8f9e81579749b3d55cefe0119f56a10
elyse502 <elyseniyibizi502@gmail.com>
2025-04-15 07:40:31 +0200

    Add Trie Implementation: Insert Method

---
+++ python/dsa/tries/trie_implementation-insert_method.py
@@ -0,0 +1,20 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_word = True
+
+    def search(self, word):
+        pass
