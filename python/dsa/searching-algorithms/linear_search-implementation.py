e9346830bac707f6aeaddc236549da4ba8d33961
elyse502 <elyseniyibizi502@gmail.com>
2025-04-17 07:29:53 +0200

    Add Linear Search Implementation

--- python/dsa/searching-algorithms/linear_search-implementation.py
+++ python/dsa/searching-algorithms/linear_search-implementation.py
@@ -9,6 +9,7 @@ def linear_search(arr, target):
 
 # Example usage
 if __name__ == "__main__":
+    # List of integers example
     arr = [10, 2, 7, 5, 3, 1, 4]
     print(f"Array: {arr}")
 
@@ -17,5 +18,22 @@ if __name__ == "__main__":
     result = linear_search(arr, target)
     print(f"\n>> Target {target} found at index {result}.")
     target = 6
+    result = linear_search(arr, target)
+    print(f">> Target {target} found at index {result}.")
+
+    # List of strings example
+    print("\n\n", "-" * 80, "\n\n\n{:_^80}".format("List of strings example:"))
+    arr = ["Eric", "Passy", "Ange", "Jane", "Peter", "Marry"]
+    print(f"\nArray: {arr}")
+
+    # Searching for different targets
+    target = "Jane"
+    result = linear_search(arr, target)
+    print(list(enumerate(arr)))
+    print("\n- Names with respective index:")
+    for idx, name in enumerate(arr):
+        print(f"\t\t\t{idx}. {name}")
+    print(f"\n>> Target {target} found at index {result}.")
+    target = "John"
     result = linear_search(arr, target)
     print(f">> Target {target} found at index {result}.")
