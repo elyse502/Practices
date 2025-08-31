228dccc766afb9c89ac005d8b3479caa6df0d061
elyse502 <elyseniyibizi502@gmail.com>
2025-08-31 07:02:48 +0200

    feat: Personalize app and add Vercel config for deployment

--- react/to-do-list/src/App.jsx
+++ react/to-do-list/src/App.jsx
@@ -1,11 +1,23 @@
-import Todo from "./Components/Todo"
+import Todo from "./Components/Todo";
 
 const App = () => {
   return (
     <div>
       <Todo />
+      <footer>
+        <p style={{ textAlign: "center", color: "white" }}>
+          Made with ❤️ by{" "}
+          <a
+            target="_blank"
+            style={{ color: "#ff6739", textDecoration: "underline" }}
+            href="https://elyse502.github.io/Elysee-Portfolio/"
+          >
+            ElyséeDev
+          </a>
+        </p>
+      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
